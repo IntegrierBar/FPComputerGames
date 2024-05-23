9ff699a34b0f29b4c74c3efd791666a1aafb41ec
Locretia <kw0498@gmx.de>
2024-05-23 17:00:18 +0200

    Main menu and assets added
    Only buttons, no connections yet

---
+++ TheMagicApprentice/modules/ui/main_menu/main_menu.gd
@@ -0,0 +1,9 @@
+class_name MainMenu
+extends Control
+
+@onready var new_game_button = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/NewGameButton as Button
+@onready var continue_button = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/ContinueButton as Button
+@onready var settings_button = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/SettingsButton as Button
+@onready var exit_button = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/ExitButton as Button
+
+
