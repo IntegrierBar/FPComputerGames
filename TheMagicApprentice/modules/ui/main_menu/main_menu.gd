3272f8968f91af6ae9c8db4db53390ad17d5f4b1
Locretia <kw0498@gmx.de>
2024-05-24 13:46:37 +0200

    Connect Main Menu and added Settings menu

--- TheMagicApprentice/modules/ui/main_menu/main_menu.gd
+++ TheMagicApprentice/modules/ui/main_menu/main_menu.gd
@@ -1,9 +1,21 @@
 class_name MainMenu
 extends Control
 
-@onready var new_game_button = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/NewGameButton as Button
-@onready var continue_button = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/ContinueButton as Button
-@onready var settings_button = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/SettingsButton as Button
-@onready var exit_button = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/ExitButton as Button
+
+func _on_new_game_button_pressed():
+	print("New game")
 
 
+func _on_continue_button_pressed():
+	print("Continue")
+
+
+# Note: the settings menu is rendered after the main menu and therefore visisble
+# without the ColorRect as background, the main menu would be visisble behind the settings menu 
+func _on_settings_button_pressed():
+	var settings = load("res://modules/ui/settings_menu/settings_menu.tscn").instantiate()
+	get_tree().current_scene.add_child(settings) 
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
