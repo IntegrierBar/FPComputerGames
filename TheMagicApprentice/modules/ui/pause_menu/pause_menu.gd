9972d37d268b27caf43efbc7d75a3326c78e760e
Locretia <kw0498@gmx.de>
2024-05-30 13:04:50 +0200

    Pause menu can be used from inside the dungeon
    Pause menu can be opened with esc key and works from inside the dungeon as well. Opening the pause menu pauses the game.

--- TheMagicApprentice/modules/ui/pause_menu/pause_menu.gd
+++ TheMagicApprentice/modules/ui/pause_menu/pause_menu.gd
@@ -2,15 +2,41 @@ class_name PauseMenu
 extends Control
 
 
+@onready var from_main_hub = false  # to remember where the pause menu is called from
+
+
+func _ready(): 
+	get_tree().paused = true
+	if (get_parent() is MainHub):
+		from_main_hub = true
+
+
+func _process(delta):
+	testEsc()
+
+
+func testEsc():
+	if Input.is_action_just_pressed("esc"):
+		resume()
+
 
 func _on_return_button_pressed():
+	resume()
+
+
+func resume():
+	get_tree().paused = false
 	queue_free()
 
 
 func _on_settings_button_pressed():
 	var settings = load("res://modules/ui/settings_menu/settings_menu.tscn").instantiate()
-	get_tree().current_scene.add_child(settings) 
+	add_child(settings) 
 
 
 func _on_exit_button_pressed():
-	get_tree().change_scene_to_file("res://modules/ui/main_menu/main_menu.tscn")
+	get_tree().paused = false
+	if from_main_hub:
+		get_tree().change_scene_to_file("res://modules/ui/main_menu/main_menu.tscn")
+	else:
+		get_tree().change_scene_to_file("res://modules/ui/main_hub/main_hub.tscn")
--- TheMagicApprentice/modules/ui/settings_menu/settings_menu.gd
+++ TheMagicApprentice/modules/ui/settings_menu/settings_menu.gd
@@ -2,6 +2,9 @@ class_name SettingsMenu
 extends Control
 
 
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
 
 # Removes the settings menu from the scene tree 
 # This means: when the settings menu is called, it always needs to be added to the current scene as a child!
@@ -10,7 +13,8 @@ extends Control
 func _on_exit_button_pressed():
 	queue_free()
 
-# Note: 
+
+# Note: there are two fullscreen options in Godot. I chose this one at random, because I dont see a difference.
 func _on_fullscreen_check_toggled(toggled_on):
 	if toggled_on:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
