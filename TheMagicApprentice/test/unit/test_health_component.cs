fd4d7c15adf3a03ae5c9f157ad16dbfb0505c241
IntegrierBar <david.seyboldt@web.de>
2024-06-01 14:46:41 +0200

    Added testing of Death signal emition

--- TheMagicApprentice/test/unit/test_health_component.cs
+++ TheMagicApprentice/test/unit/test_health_component.cs
@@ -12,6 +12,8 @@ public class HealthComponenTest
     private HealthComponent _target;
     private HealthComponent _attackingNode;
 
+    private bool died = false;
+
     // This gets called before each test
     [BeforeTest]
     public void SetupTest()
@@ -21,6 +23,7 @@ public class HealthComponenTest
         _attackingNode = AutoFree(new HealthComponent());
         _attackingNode.SetMaxHP(100.0);
         _attackingNode.SetArmor(50.0, 50.0, 50.0);
+        _target.Death += () => died = true; // Add custom method to check if Death Signal got emitted
     }
 
 /*
@@ -41,6 +44,8 @@ public class HealthComponenTest
     [TestCase(50.0, 40.0, 20.0, 10.0, 10.0, MagicType.COSMIC, 50.0 - 8.0, 100.0, TestName = "Armor Cosmic")]
     [TestCase(10.0, 120.0, 30.0, 90.0, 5.0, MagicType.DARK, 10.0 - 0.5, 100.0, TestName = "Armor Dark")]
     [TestCase(10.0, 120.0, 30.0, 90.0, 100.0, MagicType.SUN, 10.0, 100.0 - 10.0, TestName = "Test Reflection")]
+    [TestCase(10.0, 0.0, 30.0, 90.0, 10.0, MagicType.SUN, 10.0-10.0, 100.0, TestName = "Death Emitted when Health=0")]
+    [TestCase(10.0, 0.0, 30.0, 90.0, 20.0, MagicType.SUN, 10.0-20.0, 100.0, TestName = "Death Emitted when Health<0")]
     public void TestTakeDamageFunctionAsync(double maxHP, double armorSun, double armorCosmic, double armorDark, double damage, MagicType magicType, double resultHP, double attackerHP)
     {
         Attack attack = new Attack(damage, magicType, _attackingNode);
@@ -53,5 +58,7 @@ public class HealthComponenTest
         // IMPORTANT!!!!!!!!!!!!!!! DO NOT USE .Equals!!!!!! instead us .IsEqual!!!!!!!!!!!!!
         AssertThat(_target.GetCurrentHP()).IsEqual(resultHP);
         AssertThat(_attackingNode.GetCurrentHP()).IsEqual(attackerHP);
+
+        AssertBool(died).IsEqual(resultHP<=0);
     }
 }
