e9ed2b6b4509fef6872572d2c2ca4892afac3be7
IntegrierBar <david.seyboldt@web.de>
2024-06-01 15:41:16 +0200

    Implemented base State and StateMachine
    No need for unit tests, as nothing here does anything by itself.
    State is basically an abstract class and StateMachine does not implement any logic

---
+++ TheMagicApprentice/modules/entities/State.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class State : Node
+{
+	/// Reference to the parent of the scene, i.e. the entity that this state machine belongs to.
+	public CharacterBody2D Parent;
+
+	/// Reference to the AnimationPlayer of the entity
+	public AnimationPlayer Animations;
+
+
+	public virtual void Enter() {}
+
+	public virtual void Exit() {}
+
+	public virtual State ProcessInput(InputEvent @event) 
+	{
+		return null;
+	}
+
+	public virtual State ProcessFrame(double delta)
+	{
+		UpdateAnimations();
+		return null;
+	}
+
+	public virtual State ProcessPhysics(double delta)
+	{
+		return null;
+	}
+
+	/// called in ProcessFrame. Is resposible for playing the correct animation for the state
+	public virtual void UpdateAnimations() {}
+}
