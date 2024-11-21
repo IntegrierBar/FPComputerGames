7b62084dff9df640ea5f142381f2e94dd00a0b64
IntegrierBar <david.seyboldt@web.de>
2024-11-21 11:25:49 +0100

    Created new game scene

---
+++ TheMagicApprentice/modules/ui/new_game_menu/NewGameMenu.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public partial class NewGameMenu : BaseMenu
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	/**
+	Gets called when the button for the sun element is pressed
+	*/
+	public void SunSelected()
+	{}
+
+	/**
+	Gets called when the button for the cosmic element is pressed
+	*/
+	public void CosmicSelected()
+	{
+
+	}
+
+	/**
+	Gets called when the button for the dark element is pressed
+	*/
+	public void DarkSelected()
+	{
+
+	}
+}
